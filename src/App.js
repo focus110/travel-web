02805b70aa244cb868f8a29b3a10dbc5160107ff
focus110 <iamemmaochuko@gmail.com>
2023-11-27 21:50:21 +0100

    created lattest article component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import LatestArticle from "./page/LatestArticle";
 import { Home } from "./page/index";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <LatestArticle />
     </div>
   );
 }
