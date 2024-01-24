064e76c0c83ec172e4c2f71e45088f0d6c3dc4a0
focus110 <iamemmaochuko@gmail.com>
2024-01-24 04:39:05 +0100

    added text and overlay to hero section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,21 @@ const Banner = () => {
   const bannerImg =
     "https://cdn.pixabay.com/photo/2020/07/12/16/40/paris-5397889_1280.jpg";
   return (
-    <div>
-      <img className="w-full h-[500px] object-cover" src={bannerImg} alt="" />
+    <div
+      className="h-screen-minus relative"
+      style={{
+        backgroundImage: `url(${bannerImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="relative z-10 pt-[300px]">
+        <h1 className="text-white text-4xl md:text-8xl font-bold">
+          Colchester
+        </h1>
+        <p className="text-white text-xl md:text-4xl">Adventure Tours</p>
+      </div>
+      <div className="absolute top-0 bg-black w-full h-full opacity-[15%]"></div>
     </div>
   );
 };
